90e44421f90af49bc2c7566d3aa48f9cf256ee6e
speedzenon <18160216+speedzenon@users.noreply.github.com>
2026-06-29 14:01:31 +0200

    refactor: delete-gw-storage (#24)

--- infrastructure/mikrotik/main.tf
+++ infrastructure/mikrotik/main.tf
@@ -18,7 +18,7 @@ resource "routeros_interface_vlan" "this" {
 }
 
 resource "routeros_ip_address" "gw" {
-  for_each = local.vlans
+  for_each = { for k, v in local.vlans : k => v if try(v.routed, true) }
 
   address   = "${each.value.gateway}/24"
   interface = routeros_interface_vlan.this[each.key].name
