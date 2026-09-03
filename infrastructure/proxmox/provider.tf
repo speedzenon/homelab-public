8db99a550eb136e8bdd0c68b4389fb7e4fb62eab
speedzenon <18160216+speedzenon@users.noreply.github.com>
2026-09-03 08:50:21 +0200

    chore: tofu fmt (#40)

--- infrastructure/proxmox/provider.tf
+++ infrastructure/proxmox/provider.tf
@@ -3,11 +3,11 @@
 # Wstrzykiwane przez `sops exec-env` z secrets.sops.yaml.
 # api_token (format user@realm!id=uuid) zamiast hasla; insecure=true (cert self-signed).
 provider "proxmox" {
-    # Import dysku (qm disk import) idzie po SSH na noda - wymagane do tworzenia VM z obrazu.
-    # Token nie ma hasla do dziedziczenia, wiec klucz z ssh-agenta + user root.
+  # Import dysku (qm disk import) idzie po SSH na noda - wymagane do tworzenia VM z obrazu.
+  # Token nie ma hasla do dziedziczenia, wiec klucz z ssh-agenta + user root.
   ssh {
-    agent    = true
-    username = "root"
+    agent       = true
+    username    = "root"
     private_key = file(pathexpand("~/.ssh/id_ed25519"))
   }
 }
--- infrastructure/proxmox/talos.tf
+++ infrastructure/proxmox/talos.tf
@@ -22,9 +22,9 @@ locals {
 resource "proxmox_virtual_environment_download_file" "talos" {
   for_each = toset([for n in local.lab_nodes : local.pve_node[n.host]])
 
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = each.key
+  content_type            = "iso"
+  datastore_id            = "local"
+  node_name               = each.key
   url                     = "https://factory.talos.dev/image/${var.talos_schematic_id}/v${var.talos_version}/nocloud-amd64.raw.zst"
   decompression_algorithm = "zst"
   file_name               = "talos-v${var.talos_version}-nocloud-amd64.img"
@@ -36,18 +36,18 @@ module "talos_lab" {
   source   = "./modules/vm"
   for_each = local.lab_nodes
 
-  node_name     = local.pve_node[each.value.host]
-  vm_id         = each.value.vmid
-  name          = each.key
-  tags          = ["tofu", "talos", "lab", "k8s"]
-  cores         = 4
-  memory        = 8192
-  disk_size     = 40
-  datastore_id  = "local-zfs"
-  image_file_id = proxmox_virtual_environment_download_file.talos[local.pve_node[each.value.host]].id
-  ip_cidr       = "${each.value.ip}/${local.k8s_prefix}"
-  gateway       = local.k8s_vlan.gateway
-  vlan_id       = local.k8s_vlan.id
+  node_name       = local.pve_node[each.value.host]
+  vm_id           = each.value.vmid
+  name            = each.key
+  tags            = ["tofu", "talos", "lab", "k8s"]
+  cores           = 4
+  memory          = 8192
+  disk_size       = 40
+  datastore_id    = "local-zfs"
+  image_file_id   = proxmox_virtual_environment_download_file.talos[local.pve_node[each.value.host]].id
+  ip_cidr         = "${each.value.ip}/${local.k8s_prefix}"
+  gateway         = local.k8s_vlan.gateway
+  vlan_id         = local.k8s_vlan.id
   storage_vlan_id = local.net.vlans.storage.id
   storage_mac     = each.value.storage_mac
 }
